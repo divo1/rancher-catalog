02e273e439016eacab3a56ed8f0d9ca088767cd5
divo <divo@divo.net.pl>
2017-02-06 18:15:29 +0100

    Poprawki

--- templates/samba/0/samba.sh
+++ templates/samba/0/samba.sh
@@ -18,6 +18,8 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+echo "11111" > /srv2/aaaa
+
 ### charmap: setup character mapping for file/directory names
 # Arguments:
 #   chars) from:to character mappings separated by ','
